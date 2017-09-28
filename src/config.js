ba060076e26296176c33bc3d83718cf90119984a
Caleb Brewer <caleb@cazzer.io>
2017-09-28 14:40:37 -0700

    fixing pg connection string

--- src/config.js
+++ src/config.js
@@ -4,6 +4,6 @@ const DB_PASSWORD = process.env.DB_PASSWORD
 const DB_ADDRESS = process.env.DB_ADDRESS || 'localhost'
 const DB_PORT = process.env.DB_PORT || 5432
 
-const DB_ENDPOINT = `postgresql://${DB_USERNAME}:${DB_PASSWORD}@${DB_ADDRESS}:${DB_PORT}/${DB_NAME}`
+const DB_ENDPOINT = `postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_ADDRESS}:${DB_PORT}/${DB_NAME}`
 
 export { DB_ENDPOINT }
